2be616ce4471d82aaf8fadbd9a5c12cbd408a571
frort <87010782+kienn-HCl@users.noreply.github.com>
2025-03-29 04:27:27 +0900

    nixでパッケージのビルドや実行ができるようにした。

--- flake.nix
+++ flake.nix
@@ -3,10 +3,32 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ nixpkgs, flake-parts, ... }:
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ inputs.treefmt-nix.flakeModule ];
       systems = [
@@ -22,11 +44,33 @@
           pkgs,
           ...
         }:
+        let
+          workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+          overlay = workspace.mkPyprojectOverlay {
+            sourcePreference = "wheel";
+          };
+          python = pkgs.python313;
+          pythonSet =
+            (pkgs.callPackage pyproject-nix.build.packages {
+              inherit python;
+            }).overrideScope
+              (
+                pkgs.lib.composeManyExtensions [
+                  pyproject-build-systems.overlays.default
+                  overlay
+                ]
+              );
+          inherit (pkgs.callPackages pyproject-nix.build.util { }) mkApplication;
+        in
         {
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               uv
             ];
+          };
+          packages.default = mkApplication {
+            venv = pythonSet.mkVirtualEnv "edgetoon" workspace.deps.default;
+            package = pythonSet.edgetoon;
           };
           treefmt = {
             projectRootFile = "flake.nix";
